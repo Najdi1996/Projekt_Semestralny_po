c08cf21a3ddef497ad84a1a3c9ec16a942af775c
Dominik Najder <perel99@onet.pl>
2021-02-08 22:03:40 +0100

    Kodowanie dzień 2

--- Projekt_programowanie_obiektowe/MainWindow.xaml.cs
+++ Projekt_programowanie_obiektowe/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace Projekt_programowanie_obiektowe
     /// </summary>
     public partial class MainWindow : Window
     {
+        Choroby chorobaEdit;
         public MainWindow()
         {
             InitializeComponent();
@@ -40,12 +41,22 @@ namespace Projekt_programowanie_obiektowe
         private void btnAddChoroba_Click(object sender, RoutedEventArgs e)
         {
             Choroby choroba = new Choroby();
+            
             choroba.nr_choroby = txtNrChoroby.Text;
             choroba.opis_choroby = txtOpisChoroby.Text;
             using (PrzychodniaProjectDBEntities db = new PrzychodniaProjectDBEntities())
             {
                 db.Choroby.Add(choroba);
-                db.SaveChanges();
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (System.Data.Entity.Infrastructure.DbUpdateException ex)
+                {
+                    MessageBox.Show("Wystąpił problem z zapisem do bazy , opis błędu : " + ex.InnerException.InnerException.Message);
+                    return;
+                }
+
                 populateChorobyGrid();
                 MessageBox.Show("Informacja o chorobie dodana do bazy");
                 clearChorobyTextBoxes();
@@ -59,7 +70,99 @@ namespace Projekt_programowanie_obiektowe
 
         private void grdChoroby_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            String ChorobaID = grdChoroby.CurrentItem.ToString();
+            Choroby choroba = (Choroby)grdChoroby.CurrentItem;
+            this.chorobaEdit = choroba;
+            txtNrChoroby.Text = choroba.nr_choroby;
+            txtNrChoroby.IsEnabled = false;
+            txtOpisChoroby.Text = choroba.opis_choroby;
+            btnDeleteChoroba.IsEnabled = true;
+            //btnEditChoroba.IsEnabled = true;
+            btnAddChoroba.IsEnabled = false;
+
+        }
+
+        private void grdChoroby_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnClearChoroba_Click(object sender, RoutedEventArgs e)
+        {
+            clearChorobyTextBoxes();
+            txtNrChoroby.IsEnabled = true;
+            btnDeleteChoroba.IsEnabled = false;
+            btnEditChoroba.IsEnabled = false;
+            btnAddChoroba.IsEnabled = true;
+        }
+
+        private void btnEditChoroba_Click(object sender, RoutedEventArgs e)
+        {
+            
+            
+            using (PrzychodniaProjectDBEntities db = new PrzychodniaProjectDBEntities())
+            {
+                
+                Choroby choroba = db.Choroby.SingleOrDefault(c => c.nr_choroby == txtNrChoroby.Text);
+              
+                if (choroba != null)
+                {
+                try
+                {
+                    choroba.opis_choroby = txtOpisChoroby.Text;
+                    db.SaveChanges();
+                }
+                catch (System.Data.Entity.Infrastructure.DbUpdateException ex)
+                {
+                    MessageBox.Show("Wystąpił problem z zapisem do bazy , opis błędu : " + ex.InnerException.InnerException.Message);
+                    return;
+                }
+                }
+
+                populateChorobyGrid();
+                MessageBox.Show("Informacja o chorobie zaktualizowana w bazie");
+                clearChorobyTextBoxes();
+                txtNrChoroby.IsEnabled = true;
+            }
+        }
+
+        private void btnDeleteChoroba_Click(object sender, RoutedEventArgs e)
+        {
+            using (PrzychodniaProjectDBEntities db = new PrzychodniaProjectDBEntities())
+            {
+
+                Choroby choroba = db.Choroby.SingleOrDefault(c => c.nr_choroby == txtNrChoroby.Text);
+
+                if (choroba != null)
+                {
+                    try
+                    {
+                        db.Choroby.Remove(choroba);
+                        db.SaveChanges();
+                    }
+                    catch (System.Data.Entity.Infrastructure.DbUpdateException ex)
+                    {
+                        MessageBox.Show("Wystąpił problem z zapisem do bazy , opis błędu : " + ex.InnerException.InnerException.Message);
+                        return;
+                    }
+                }
+
+                populateChorobyGrid();
+                MessageBox.Show("Informacja o chorobie została usunięta z bazy");
+                clearChorobyTextBoxes();
+                txtNrChoroby.IsEnabled = true;
+            }
+        }
+
+        private void txtOpisChoroby_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(txtOpisChoroby.Text != this.chorobaEdit.opis_choroby)
+            {
+                btnEditChoroba.IsEnabled = true;
+            }
+            else
+            {
+                btnEditChoroba.IsEnabled = false;
+            }
         }
     }
 }
